9182934c37dcd860b859162bd938d2b1d06b1f42
l3miage-marinhon <nicomr70@gmail.com>
2021-06-10 12:18:06 +0200

    correction du main de stats pour les prochain tp

--- Parcours d'un tableau de nombres lab id 29937/src/stats/TestStats.java
+++ Parcours d'un tableau de nombres lab id 29937/src/stats/TestStats.java
@@ -3,15 +3,6 @@ package stats;
 public class TestStats {
 
     public static void main(String[] args) {
-        double[] t = { 1.5, 1.0, -5.2, -34.3 };
-        Statistiques stats = new Statistiques(t);
-        stats.afficher();
-		stats = new Statistiques(10);
-		stats.afficher();
-        System.out.println("Elément minimun du tableau: " + stats.getMin());
-        System.out.println("Elément maximum du tableau: " + stats.getMax());
-        System.out.println("Moyenne du tableau: " + stats.moyenne());
-        System.out.println("Variance du tableau: " + stats.variance());
-        System.out.println("Ecart type du tableau: " + stats.ecartType());
+    	//TODO faire les tous les test pertinent
     }
 }
