c0cdc46b52a86e74682544a784bad0e613d4bec9
FrancesTomUGA <tom.frances97@yahoo.fr>
2021-06-13 17:32:00 +0200

    Rendu TP Parcours d'un tableau de nombres

--- Parcours d'un tableau de nombres lab id 29937/src/stats/Statistiques.java
+++ Parcours d'un tableau de nombres lab id 29937/src/stats/Statistiques.java
@@ -1,36 +1,93 @@
 package stats;
 
+import java.util.Locale;
+
 class Statistiques {
 	//TODO declarer un tableau de double appartenant qu'a la class
+	private double[] tab;
+	private double max = 1000.0;
     //TODO faire le constructeur qui prend un tableau
     
+	public Statistiques(double[] t){
+    	tab = t;
+    }
     
-    //TODO faire un constructeur qui construit un tableau de double de manière rendom avec i element 
- 
+	
+    //TODO faire un constructeur qui construit un tableau de double de manière random avec i element 
+	public Statistiques(int i) {
+		tab = new double[i];
+		for(int n=0; n<i; n++) {
+			tab[n] = Math.random()*max; //remplis le tableau avec des doubles entre 0.0 et 1000.0
+		}
+	}
+	
     //TODO afficher le tableau dans la console de cette maniere [1.00,2.00]
+	public void afficheTableau() {
+		System.out.print("[");
+		int s = tab.length;
+		for(int i=0; i<s; i++) {
+			String str = String.format(Locale.US,"%.2f", tab[i]);
+			System.out.print(str);
+			if(i < s-1) System.out.print(","); 
+		}
+		System.out.println("]");
+	}
 
     double getMin() {
-    	return 0;
+    	if(tab.length>0){
+	    	double minAct = max;
+	    	for(int i=0; i<tab.length; i++) {
+	    		minAct = (tab[i] < minAct) ? tab[i] : minAct;
+	    	}
+	    	return minAct;
+	    }else{
+    		return 0.0;
+    	}
     	//TODO 
     }
 
     double getMax() {
-    	return 0;
+    	if(tab.length>0){
+	    	double maxAct = 0.0;
+	    	for(double n : tab) {
+	    		maxAct = (n > maxAct) ? n : maxAct;
+	    	}
+	    	return maxAct;
+	    }else{
+	    	return 0.0;
+	    }
     // TODO
     }
 
     double moyenne() {
-    	return 0;
+    	if(tab.length>0){
+	    	double mean = 0.0;
+	    	for(int i=0; i<tab.length; i++){
+	    		mean += tab[i];
+	    	}
+	    	return mean/(double)tab.length;
+	    }else{
+	    	return 0.0;
+	    }
     // TODO
     }
 
     double variance() {
-    	return 0;
+    	if(tab.length>0){
+	    	double var = 0.0;
+	    	double mean = moyenne();
+	    	for(double n : tab){
+	    		var += Math.pow(n-mean, 2);
+	    	}
+	    	return var/(double)tab.length;
+	    }else{
+	    	return 0.0;
+	    }
     // TODO
     }
 
     double ecartType() {
-        return 0;
+        return Math.sqrt(variance());
     // TODO
     }
 }
--- Parcours d'un tableau de nombres lab id 29937/src/stats/TestStats.java
+++ Parcours d'un tableau de nombres lab id 29937/src/stats/TestStats.java
@@ -1,8 +1,57 @@
 package stats;
+import java.util.Locale;
 
 public class TestStats {
 
     public static void main(String[] args) {
     	//TODO faire les tous les test pertinent
+    	Statistiques tableau1 = new Statistiques(10);
+    	tableau1.afficheTableau();
+    	System.out.println("max = " + String.format(Locale.US, "%.2f", tableau1.getMax()));
+    	System.out.println("min = " + String.format(Locale.US, "%.2f", tableau1.getMin()));
+    	System.out.println("moyenne = " + String.format(Locale.US, "%.2f", tableau1.moyenne()));
+    	System.out.println("variance = " + String.format(Locale.US, "%.2f", tableau1.variance()));
+    	System.out.println("ecart type = " + String.format(Locale.US, "%.2f", tableau1.ecartType()));
+    	
+    	System.out.println();
+    	
+    	double[] t = new double[] {100.00, 200.00, 250.00, 300.00, 500.00};
+    	Statistiques tableau2 = new Statistiques(t);
+    	tableau2.afficheTableau();
+    	System.out.println("max = " + String.format(Locale.US, "%.2f", tableau2.getMax()));
+    	System.out.println("min = " + String.format(Locale.US, "%.2f", tableau2.getMin()));
+    	System.out.println("moyenne = " + String.format(Locale.US, "%.2f", tableau2.moyenne()));
+    	System.out.println("variance = " + String.format(Locale.US, "%.2f", tableau2.variance()));
+    	System.out.println("ecart type = " + String.format(Locale.US, "%.2f", tableau2.ecartType()));
+    	
+    	System.out.println();
+    	
+    	Statistiques tableau3 = new Statistiques(0);
+    	tableau3.afficheTableau();
+    	System.out.println("max = " + String.format(Locale.US, "%.2f", tableau3.getMax()));
+    	System.out.println("min = " + String.format(Locale.US, "%.2f", tableau3.getMin()));
+    	System.out.println("moyenne = " + String.format(Locale.US, "%.2f", tableau3.moyenne()));
+    	System.out.println("variance = " + String.format(Locale.US, "%.2f", tableau3.variance()));
+    	System.out.println("ecart type = " + String.format(Locale.US, "%.2f", tableau3.ecartType()));
+    	
+    	System.out.println();
+    	
+    	Statistiques tableau4 = new Statistiques(1);
+    	tableau4.afficheTableau();
+    	System.out.println("max = " + String.format(Locale.US, "%.2f", tableau4.getMax()));
+    	System.out.println("min = " + String.format(Locale.US, "%.2f", tableau4.getMin()));
+    	System.out.println("moyenne = " + String.format(Locale.US, "%.2f", tableau4.moyenne()));
+    	System.out.println("variance = " + String.format(Locale.US, "%.2f", tableau4.variance()));
+    	System.out.println("ecart type = " + String.format(Locale.US, "%.2f", tableau4.ecartType()));
+    	
+    	System.out.println();
+    	
+    	Statistiques tableau5 = new Statistiques(2);
+    	tableau5.afficheTableau();
+    	System.out.println("max = " + String.format(Locale.US, "%.2f", tableau5.getMax()));
+    	System.out.println("min = " + String.format(Locale.US, "%.2f", tableau5.getMin()));
+    	System.out.println("moyenne = " + String.format(Locale.US, "%.2f", tableau5.moyenne()));
+    	System.out.println("variance = " + String.format(Locale.US, "%.2f", tableau5.variance()));
+    	System.out.println("ecart type = " + String.format(Locale.US, "%.2f", tableau5.ecartType()));
     }
 }
