78e44f630f90415f2311b9f71094c61c3eb5ae4a
FrancesTomUGA <tom.frances97@yahoo.fr>
2021-07-08 13:35:57 +0200

    Rendu TP Horaire exceptions

---
+++ Horaire exceptionnel/src/edu/uha/miage/exceptions/IncorrectHour.java
@@ -0,0 +1,10 @@
+package edu.uha.miage.exceptions;
+
+public class IncorrectHour extends Exception{
+	public IncorrectHour(String s) {
+		super(s);
+	}
+	public IncorrectHour() {
+		super();
+	}
+}
--- Horaire exceptionnel/src/edu/uha/miage/horaire/Horaire.java
+++ Horaire exceptionnel/src/edu/uha/miage/horaire/Horaire.java
@@ -17,6 +17,8 @@
  */
 package edu.uha.miage.horaire;
 
+import java.util.ArrayList;
+
 import edu.uha.miage.exceptions.*;
 /**
  * Une classe horaire pour gérer des horaires (heures, minutes, secondes).
@@ -35,7 +37,12 @@ import edu.uha.miage.exceptions.*;
  */
 public class Horaire {
 	//TODO mettre les attribue de la classes
-
+	private int f;
+	private int h;
+	private int m;
+	private int s;
+	
+	
     /**
      * TODO 1.02 Ecrire le constructeur qui construit un nouvel horaire à partir
      * des arguments h m et s.
@@ -55,6 +62,22 @@ public class Horaire {
      * @throws edu.uha.miage.exceptions.BadHoraireException si l'horaire donnée
      * est incorrect.
      */
+	public Horaire(int f, int h, int m, int s) {
+		try {
+			setF(f);
+			setH(h);
+			setM(m);
+			setS(s);
+		}catch(IncorrectFormat IF) {
+			IF.printStackTrace();
+		}catch(IncorrectHour IH) {
+	    	IH.printStackTrace();
+	    }catch(IncorrectMinute IM) {
+	    	IM.printStackTrace();
+	    }catch(IncorrectSecond IS) {
+	    	IS.printStackTrace();
+	    }
+	}
     /**
      
      * 
@@ -70,7 +93,9 @@ public class Horaire {
      * @throws edu.uha.miage.exceptions.BadHoraireException si l'horaire donnée
      * est incorrect.
      */
-    
+    public Horaire(int f, int h, int m) {
+    	this(f, h, m, 0);
+    }
     /**
      * TODO 1.04. Ecrire le constructeur qui construit un nouvel horaire à
      * partir d'un seul argument : l'heure (les secondes et les minutes sont nulles).
@@ -80,30 +105,65 @@ public class Horaire {
      *
      * @throws edu.uha.miage.exceptions.BadHoraireException si l'horaire donnée
      * est incorrect.
+    */
+    public Horaire(int f, int h) {
+    	this(f, h, 0, 0);
+    }
     /**
      * TODO 1.05. Ecrire le constructeur par défaut qui construit l'horaire
      * 00:00:00.
-     * 
-		
      */
+    public Horaire() {
+    	this(24, 0, 0, 0);
+    }
+	//TODO faire les getter et setters
+    
+	public int getF() {
+		return f;
+	}
+	private void setF(int f) throws IncorrectFormat{
+		if(f != 12 && f != 24) {
+			throw new IncorrectFormat("Mauvais format d'heure (12 ou 24)");
+		}else {
+			this.f = f;		
+		}
+	}
 	
-	//TODO faire les getter 
-    /**
-     * @return les heures de cet horaire
-     */
-
-
-    /**
-     * @return les minutes de cet horaire
-     */
-
-
-    /**
-     * @return les secondes de cet horaire
-     */
-
-
+	public int getH() {
+		return h;
+	}
+	private void setH(int h) throws IncorrectHour{
+		if( f == 24 && (h < 0 || h > 23) ){
+			throw new IncorrectHour("Erreur sur l'heure renseignée (entre 0 et 23)");
+		}else if( f == 12 && (h < 0 || h > 11) ) {
+			throw new IncorrectHour("Erreur sur l'heure renseignée (entre 0 et 11)");
+		}else {
+			this.h = h;
+		}
+	}
 	
+	public int getM() {
+		return m;
+	}
+	private void setM(int m) throws IncorrectMinute{
+		if( m < 0 || m > 59) {
+			throw new IncorrectMinute("Erreur sur les minutes renseignées (entre 0 et 59)");
+		}else {
+			this.m = m;
+		}
+	}
+	
+	public int getS() {
+		return s;
+	}
+	private void setS(int s) throws IncorrectSecond{
+		if( s < 0 || s > 59 ) {
+			throw new IncorrectSecond("Erreur sur les secondes renseignées (entre 0 et 59)");
+		}else {
+			this.s = s;
+		}
+	}
+
 	//TODO 
     /**
      * Retourne un horaire sous la forme d'une chaîne de caractères sous la
@@ -114,22 +174,29 @@ public class Horaire {
      *
      * @return un horaire de la forme "hh:mm:ss"
      */
-	
+	@Override
+    public String toString() {
+    // TODO 1.18. Écrire toString()
+    	return String.format("%02d:%02d:%02d", h, m, s) ;
+    }
     /**
      * TODO Augmente cet horaire d'une heure.
      */
+	public void uneHeureDePlus() {
+		this.h = h+1 > 23 ? 0 : h+1 ;
+	}
     /**
      * TODO Augmente cet horaire d'une minute.
      */
-
+	public void uneMinuteDePlus() {
+		this.m = m+1 > 59 ? 0 : m+1;
+	}
     /**
      * TODO Augmente cet horaire d'une seconde.
      */
-    // 
-    // 
-    // 
-    // 
-    // TODO 1.06. Écrire les setters pour les heures, les minutes et les secondes
+	public void uneSecondeDePlus() {
+		this.s = s+1 > 59 ? 0 : s+1;
+	}
     
     
     
@@ -150,6 +217,31 @@ public class Horaire {
 		sont admis "02:25" "2:25" "2:2" "12:25:02" "15:00:00" "0:0:0"
 		Ne sont pas admis : "25:00" "2h2" "13:45:62"
 		*/
-    // 
-	
+	public static Horaire horaireFromString(String s) throws WrongStringFormatSize{
+		Horaire horaire = null;
+		String elemStr[] = s.split(":");
+		ArrayList<Integer> elemInt = new ArrayList<>();
+		for(String e : elemStr) {
+			try {
+				elemInt.add(Integer.parseInt(e));
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		if(elemInt.size() < 3 || elemInt.size() > 4) {
+			throw new WrongStringFormatSize("Chaine de caractères incorrecte (il faut 3 ou 4 champs séparés par les ':' )");
+		}else {
+			int format = elemInt.get(0);
+			int heure = elemInt.get(1);
+			int minute = elemInt.get(2);
+			int seconde = 0;
+			if(elemInt.size() == 4) {
+				seconde = elemInt.get(3);
+			}
+			horaire = new Horaire(format, heure, minute, seconde);
+		}
+		return horaire;
+	}
 }
+
+
