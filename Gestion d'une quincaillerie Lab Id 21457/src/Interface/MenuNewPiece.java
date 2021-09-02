473d207bff94f9cf7ad1d750353766fbd8ad4de6
FrancesTomUGA <tom.frances97@yahoo.fr>
2021-09-02 15:54:54 +0000

    avancement MenuNewPiece

--- Gestion d'une quincaillerie Lab Id 21457/src/Interface/MenuNewPiece.java
+++ Gestion d'une quincaillerie Lab Id 21457/src/Interface/MenuNewPiece.java
@@ -3,23 +3,23 @@ package Interface;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Image;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
@@ -32,9 +32,17 @@ import main.Application;
 public class MenuNewPiece implements Runnable {
 
 	JFrame frmNewPiece;
-	JButton btnReturn;
-	JPanel pnlNewPiece;
-
+	JButton btnValider;
+	JButton btnAnnuler;
+	JPanel content;
+	JPanel pnlInfosPiece;
+	JPanel pnlBtnType;
+	
+	JLabel[] labels = {	new JLabel("Nom : "), new JLabel("Prix : "), new JLabel("Durée fabrication : "), new JLabel("Durée garantie : ")};
+	JComponent[] fields = { new JTextField(14), new JTextField(14), new JTextField(14), new JTextField(14)};
+	
+	ArrayList<JLabel> labelsA = new ArrayList<>();
+	ArrayList<JComponent> fieldsA = new ArrayList<>();
 	
 	public static final String PATH_TO_ICONS = "src/icons/";
 	
@@ -67,7 +75,7 @@ public class MenuNewPiece implements Runnable {
 		System.out.println(Application.clientCourant);
 		initUI();
 		frmNewPiece = new JFrame();
-		frmNewPiece.setSize(new Dimension(1200, 800));
+		frmNewPiece.setSize(new Dimension(600, 500));
 		frmNewPiece.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		frmNewPiece.setLocationRelativeTo(null);
 		frmNewPiece.setMinimumSize(new Dimension(500, 300));
@@ -81,49 +89,58 @@ public class MenuNewPiece implements Runnable {
 				}
 			}
 		});
-		JPanel content = (JPanel) frmNewPiece.getContentPane();
+		content = (JPanel) frmNewPiece.getContentPane();
 		content.setLayout(new BorderLayout());
+		ArrayList<Integer> kj = new ArrayList<>();
+		kj.add(3);
+		kj.add(4);
+		System.out.println(kj);
+		kj.clear();
+		System.out.println(kj);
+		content.add(typePiece(true), BorderLayout.NORTH);
 		
-		//content.add(new JLabel("Chargement"), BorderLayout.CENTER);
+		pnlInfosPiece = new JPanel();
+		pnlInfosPiece.add(infosPiece(true));
+		content.add(pnlInfosPiece, BorderLayout.CENTER);
+
 		frmNewPiece.setVisible(true);
 		frmNewPiece.setTitle("Nouvelle pièce");
 
-		content.add(createBtnReturn(), BorderLayout.NORTH);
-		
+		content.add(createPanelValiderAnnuler(labels, fields, true), BorderLayout.SOUTH);		
 	}
 	
-	private JPanel nouvellePiece() {
-		pnlNewPiece = new JPanel(new BorderLayout());
-
-		JPanel pnlTypePiece = typePiece();
-		pnlInfosPiece = infosPiece(true);
+	private void refreshFrame(boolean isTypeBase) {
+		frmNewPiece.getContentPane().removeAll();
+		content = (JPanel) frmNewPiece.getContentPane();
+		content.setLayout(new BorderLayout());
 		
-		content.add(pnlTypePiece, BorderLayout.NORTH);
+		content.add(typePiece(isTypeBase), BorderLayout.NORTH);
+		
+		pnlInfosPiece = new JPanel();
+		pnlInfosPiece.add(infosPiece(isTypeBase));
 		content.add(pnlInfosPiece, BorderLayout.CENTER);
-		
-		return newPiece;
+		content.add(createPanelValiderAnnuler(labels, fields, isTypeBase), BorderLayout.SOUTH);
 	}
 	
-	private JPanel typePiece() {
+	private JPanel typePiece(boolean isTypeBase) {
 		JPanel typePiece = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		ButtonGroup btnGrp = new ButtonGroup();
 		JRadioButton b1 = new JRadioButton("Pièce de base");
 		b1.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 20));
-		b1.setSelected(true);
 		b1.addActionListener(ev->{
-			System.out.println("aze");
-			pnlInfosPiece.removeAll();
-			pnlInfosPiece.add(infosPiece(true));
-			pnlInfosPiece.revalidate();
+			System.out.println("base");
+			refreshFrame(true);
+			frmNewPiece.revalidate();
 		});
 		JRadioButton b2 = new JRadioButton("Pièce composite");
 		b2.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
 		b2.addActionListener(ev->{
-			System.out.println("qsd");
-			pnlInfosPiece.removeAll();
-			pnlInfosPiece.add(infosPiece(false));
-			pnlInfosPiece.revalidate();
+			System.out.println("composite");
+			refreshFrame(false);
+			frmNewPiece.revalidate();
 		});
+		(isTypeBase ? b1 : b2).setSelected(true);
+		
 		btnGrp.add(b1);
 		btnGrp.add(b2);
 
@@ -135,22 +152,100 @@ public class MenuNewPiece implements Runnable {
 	
 	private JPanel infosPiece(Boolean base) {
 		JPanel infosPiece = new JPanel();
+		infosPiece.setLayout(new BoxLayout(infosPiece, BoxLayout.Y_AXIS));
 		
 		if(base) {
-			infosPiece.add(new JLabel("base"));
+			infosPiece.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+			infosPiece.add(partMenu(labels, fields));
+			
 		}else {
-			infosPiece.add(new JLabel("composite"));
+			infosPiece.add(new JLabel(" kjh"));
 		}
 		
 		return infosPiece;
 	}
 	
-	private JPanel createBtnReturn() {
-		JPanel pnlBtnReturn = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		btnReturn = new JButton(new ImageIcon(new ImageIcon(PATH_TO_ICONS + "return_icon.png").getImage().getScaledInstance(20, 15, Image.SCALE_SMOOTH)));
-		btnReturn.addActionListener(ev->{MenuQuincailleriePieces.demarrer(frmNewPiece);});
-		pnlBtnReturn.add(btnReturn);
-		return pnlBtnReturn;
+	private JPanel partMenu(JLabel[] labels, JComponent[] fields) {
+		if (labels.length != fields.length) {
+	    	String s = labels.length + " labels supplied for " + fields.length + " fields!";
+	        throw new IllegalArgumentException(s);
+		}
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		int n = 0;
+		for(JLabel label : labels) {
+			gbc.gridx = 0;
+			gbc.gridy = n;
+			gbc.anchor = GridBagConstraints.WEST;
+			panel.add(label, gbc);
+			n++;
+		}
+		
+		n = 0;
+		for(JComponent field : fields) {
+			gbc.gridx = 1;
+			gbc.gridy = n;
+			gbc.anchor = GridBagConstraints.WEST;
+			panel.add(field, gbc);
+			n++;
+		}
+		 
+		return panel;
+	}	
+	
+	private JPanel createPanelValiderAnnuler(JLabel[] labels, JComponent[] fields, boolean isTypeBase) {
+		JPanel pnlValCan = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		//btnReturn = new JButton(new ImageIcon(new ImageIcon(PATH_TO_ICONS + "return_icon.png").getImage().getScaledInstance(20, 15, Image.SCALE_SMOOTH)));
+		
+		btnValider = createBtnValider(isTypeBase);
+		
+		btnAnnuler = new JButton("Annuler");
+		btnAnnuler.addActionListener(ev->{System.out.println("annuler");});
+		pnlValCan.add(btnAnnuler);
+		pnlValCan.add(btnValider);
+		
+		return pnlValCan;
+	}
+	
+	private JButton createBtnValider(boolean isTypeBase) {
+		JButton btnValider = new JButton("Valider");
+		if(isTypeBase) {
+			btnValider.addActionListener(ev->{
+				System.out.println("valider base");
+				
+				String nom = ((JTextField) fields[0]).getText();
+				
+				Double prix = null;
+				try {
+					prix = Double.parseDouble(((JTextField) fields[1]).getText());
+				} catch(NumberFormatException e) {
+					System.out.println("Erreur format prix");
+				}
+				
+				Integer dFab = null;
+				try {
+					dFab = Integer.parseInt(((JTextField) fields[2]).getText());
+				} catch(NumberFormatException e) {
+					System.out.println("Erreur format durée fabrication");
+				}
+				
+				Integer dGar = null;
+				try {
+					dGar = Integer.parseInt(((JTextField) fields[3]).getText());
+				} catch(NumberFormatException e) {
+					System.out.println("Erreur format durée garantie");
+				}
+				
+				System.out.println( nom + " " + prix + " " + dFab + " " + dGar);
+				
+			});
+		}else {
+			btnValider.addActionListener(ev->{
+				System.out.println("valider comp");
+			});
+		}
+		return btnValider;
 	}
 	
 }
